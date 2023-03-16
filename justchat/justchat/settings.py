18cf46ed72099f434e8dd2a43f28056d11edae96
akattipa <akattipa>
2023-03-16 14:27:34 +0530

    modified for postgres support

--- justchat/justchat/settings.py
+++ justchat/justchat/settings.py
@@ -78,10 +78,21 @@ ASGI_APPLICATION = "justchat.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',#database name
+        'USER': '',#db_username
+        'PASSWORD': '',#db_password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
